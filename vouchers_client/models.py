82a06caa68a681fcdd000c36d50ecd52136af7b0
Sergio <oliveira-sergio@outlook.com>
2026-01-25 10:58:42 +0000

    Update models.py
    act:
    -> model created (to get the table vouchers created by the admin-side)
    -> the method display the vouchers created

--- vouchers_client/models.py
+++ vouchers_client/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from boto3.dynamodb.conditions import Key
+
+import boto3
 
 # Create your models here.
+class Voucher(models.Model):
+
+    # AWS Details
+    _TABLE_NAME = 'vouchers_db'
+    _REGION = 'eu-west-1'
+
+    @classmethod
+    def _get_table(cls):
+        dynamodb = boto3.resource('dynamodb', region_name=cls._REGION)
+        return dynamodb.Table(cls._TABLE_NAME)
+
+    @staticmethod
+    def list_vouchers_by_location(location_code):
+        table = Voucher._get_table()
+        response = table.query(KeyConditionExpression=Key('voucher_location').eq(location_code))
+
+        return response.get('Items', [])
