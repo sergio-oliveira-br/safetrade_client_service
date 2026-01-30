19bfca244cde12c80a475c7c1934ac1c44981af2
Sergio <oliveira-sergio@outlook.com>
2026-01-30 23:04:51 +0000

    Update urls.py
    act:
    -> route added

--- vouchers_client/urls.py
+++ vouchers_client/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Index
     path('', views.index_page , name='index'),
     path('showroom/', views.showroom, name='showroom'),
+    path('voucher_checkout/<str:voucher_id>/', views.view_voucher_checkout, name='voucher_checkout'),
 ]
