fec91937d7ec90022c07b2bc6f3c17b1f0dccebc
Sergio <oliveira-sergio@outlook.com>
2026-02-02 10:54:11 +0000

    Create web3_send_tx_hash_to_backend.js
    act:
    -> method created
    
    info:
    this method will be responsible to send the tx hash to the backend via AJAX

---
+++ vouchers_client/static/vouchers_client/js/web3_send_tx_hash_to_backend.js
@@ -0,0 +1,30 @@
+// vouchers_client/static/vouchers_client/js/web3_send_tx_hash_to_backend.js
+
+async function sendHashToDjango(tx_hash) {
+
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+    try {
+        const response = await fetch('/update_voucher_tx_hash/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify({
+                'tx_hash': tx_hash,
+                 // 'voucher_id': voucher_id
+            })
+        });
+
+        if (!response.ok) {
+            console.error(response.json())
+            }
+
+        else {
+            console.error("Error communicating with Django server.");
+        }
+    } catch (error) {
+        console.error("Error in Fetch request:", error);
+    }
+}
