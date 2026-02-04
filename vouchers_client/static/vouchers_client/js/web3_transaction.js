c88669ef2522952fa141acafc73507ad1138bc87
Sergio <oliveira-sergio@outlook.com>
2026-02-04 08:46:24 +0000

    Update web3_transaction.js
    method refactored
    
    act:
    -> verification if the user is connected to metamask has been improved

--- vouchers_client/static/vouchers_client/js/web3_transaction.js
+++ vouchers_client/static/vouchers_client/js/web3_transaction.js
@@ -3,47 +3,38 @@ const btn_by_voucher = document.getElementById('buy_voucher')
 
 async function buy_voucher() {
 
-const metamask_provider = window.ethereum
-const is_connected = metamask_provider.isConnected();
+    const metamask_provider = window.ethereum
 
-    if(metamask_provider.isMetaMask){
+    if(await is_connected()) {
+        try {
+            const accounts = await metamask_provider.request({
+                method: 'eth_requestAccounts'
+            });
 
-        if(is_connected === true){
+            const account = accounts[0]
 
-            try {
-                const accounts = await metamask_provider.request({
-                    method: 'eth_requestAccounts'
-                });
-
-                const account = accounts[0]
-
-                // https://docs.metamask.io/wallet/reference/provider-api#disconnect
-                const transactionParameters = {
-                    from: account,
-                    to: "0x29E70456CE821A009492FCB44232D67b042A1B49",
-                    value: "0",
-                    chainId: "0xaa36a7"
-                }
-               const tx_hash = await metamask_provider.request({
-                    method: "eth_sendTransaction",
-                    params: [transactionParameters]
-                ,});
-
-                alert("tx_hash: " + tx_hash)
-
-                await sendHashToDjango(tx_hash)
-
-            } catch(error) {
-                console.error(error)
-                alert("Erro: " +  error.message)
+            // https://docs.metamask.io/wallet/reference/provider-api#disconnect
+            const transactionParameters = {
+                from: account,
+                to: "0x29E70456CE821A009492FCB44232D67b042A1B49",
+                value: "0",
+                chainId: "0xaa36a7"
             }
+            const tx_hash = await metamask_provider.request({
+                method: "eth_sendTransaction",
+                params: [transactionParameters]
+            });
+
+            alert("tx_hash: " + tx_hash)
+
+            await sendHashToDjango(tx_hash)
+
+        } catch (error) {
+            console.error(error)
+            alert("Erro: " + error.message)
         }
-        else{
-            alert("Account is not connected")
-        }
-    }
-    else{
-        alert("Sorry! MetaMask not detected, please install the extension.");
+    } else {
+        alert("Please connect to your MetaMask Account before attempt to buy a voucher")
     }
 }
 
