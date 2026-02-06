aad24c2d42cfead813f649c614ce6195166bdc20
Sergio <oliveira-sergio@outlook.com>
2026-02-06 16:31:24 +0000

    Create aws_sqs_hash_validation.py
    act:
    -> method to send hash to SQS created

---
+++ vouchers_client/aws_sqs_hash_validation.py
@@ -0,0 +1,33 @@
+# vouchers_client/aws_sqs_hash_validation.py
+import json
+import logging
+import boto3
+
+# AWS Details
+REGION = 'eu-west-1'
+SQS_QUEUE_URL = 'https://sqs.eu-west-1.amazonaws.com/542672133770/payment-processing-queue'
+client = boto3.client('sqs', region_name=REGION)
+
+class SQSHashValidation:
+
+    @staticmethod
+    def send_hash_to_sqs() -> bool | None:
+
+        if not client:
+            logging.error('The message can not be delivered, no SQS client configured')
+            return False
+
+        message_payload = {
+            'tx_hash': '0x999ab5ee766042609d931158bb28857b019e4cff3ebc787fb07228b747f25b9f'
+        }
+
+        try:
+            sqs_response = client.send_message(
+                QueueUrl=SQS_QUEUE_URL,
+                MessageBody=json.dumps(message_payload)
+            )
+            return True
+
+        except Exception as e:
+            logging.error(e)
+            return False
