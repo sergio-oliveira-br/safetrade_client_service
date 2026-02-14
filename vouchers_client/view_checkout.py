6a89d75735da254f6e50388b6c17ac908eb762b7
Sergio <oliveira-sergio@outlook.com>
2026-02-14 20:05:49 +0000

    Update view_checkout.py

--- vouchers_client/view_checkout.py
+++ vouchers_client/view_checkout.py
@@ -2,6 +2,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.http import require_http_methods
+
 from vouchers_client.services.aws_sqs_service import SQSService
 from vouchers_client.services.aws_dynamo_service import VoucherDynamoService
 from vouchers_client.services.orchestrator_service import VoucherCheckoutOrchestrator
@@ -20,11 +22,15 @@ def voucher_checkout_page(request, voucher_id):
 
 @csrf_protect
 def voucher_checkout_update(request):
+
+    voucher_id = request.POST.get('voucher_id')
+    voucher_tx_hash = request.POST.get('tx_hash_input')
+
     # Instantiate the Orchestrator Service
     orchestrator_service = VoucherCheckoutOrchestrator()
 
     # Use the instance to handle the business logic
-    response = orchestrator_service.handle_checkout_update(request.body)
+    response = orchestrator_service.handle_checkout_update(voucher_id, voucher_tx_hash)
     status_code = 200 if response['status_code'] else 400
 
-    return JsonResponse(response, status=status_code)
+    return render(request, 'core/voucher_checkout.html', context={'voucher': voucher_id})
