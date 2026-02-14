8487f368eb8cd2419614a496bec88c315ffacb97
Sergio <oliveira-sergio@outlook.com>
2026-02-14 19:05:42 +0000

    Update web3_send_tx_hash_to_backend.js
    code refactored

--- vouchers_client/static/vouchers_client/js/web3_transaction.js
+++ vouchers_client/static/vouchers_client/js/web3_transaction.js
@@ -1,35 +1,45 @@
 // vouchers_client/static/vouchers_client/js/web3_transaction.js
 
-const btn_by_voucher = document.getElementById('buy_voucher')
+const btn_by_voucher = document.getElementById('buyVoucher')
 
-async function buy_voucher() {
+async function connectWallet() {
+    const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts'
+    });
+    return accounts[0];
+}
+
+async function sendTransactionToMetaMask(account) {
+    const transactionParameters = {
+        from: account,
+        to: "0x29E70456ce821A009492FcB44232D67b042a1B49",
+        value: "0",
+        chainId: "0xaa36a7"
+    };
+
+    return await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters]
+    });
+}
+
+function updateUI(tx_hash) {
+    alert("We are processing your payment. Please wait few seconds!");
+    console.log("Transaction Hash:", tx_hash);
+}
+
+async function buyVoucher() {
     try {
-        const accounts = await window.ethereum.request({
-            method: 'eth_requestAccounts'
-        });
-        const account = accounts[0];
+        const account = await connectWallet();
+        const tx_hash = await sendTransactionToMetaMask(account);
+        updateUI(tx_hash);
 
-        // https://docs.metamask.io/wallet/reference/provider-api#disconnect
-        const transactionParameters = {
-            from: account,
-            to: "0x29E70456ce821A009492FcB44232D67b042a1B49",
-            value: "0",
-            chainId: "0xaa36a7"
-        }
-        const tx_hash = await window.ethereum.request({
-            method: "eth_sendTransaction",
-            params: [transactionParameters]
-        });
-
-        alert("We are processing your payment. Please wait few seconds!")
-
-        await sendHashToDjango(tx_hash)
-        reload_page();
+        // await sendHashToDjango(tx_hash);
 
     } catch (error) {
-        console.error(error)
-        alert("Erro: " + error.message)
+        console.error(error);
+        alert("Error: " + error.message);
     }
 }
 
-btn_by_voucher.onclick = buy_voucher
+btn_by_voucher.onclick = buyVoucher
